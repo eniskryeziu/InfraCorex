c903adc3e99e447476ac9b17e785852117e3c1e4
Enis Kryeziu <eniskryeziu539@gmail.com>
2026-04-27 16:11:58 +0200

    InfraCorex: Infrastructure update (Version 3)

--- main.tf
+++ main.tf
@@ -3,10 +3,8 @@ provider "aws" {
   region  = "eu-north-1"
 }
 
-
 resource "aws_s3_bucket" "app_bucket_infra_corex_enis" {
-  bucket = var.s3_bucket_name
-  acl    = "private"
+  bucket = "app_bucket_infra_corex_enis"
 
   tags = {
     Name  = "app_bucket_infra_corex_enis"
