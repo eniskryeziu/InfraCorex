125230e00f4e0b3627ad929c39be3928e0c088eb
Enis Kryeziu <eniskryeziu539@gmail.com>
2026-04-27 13:07:19 +0200

    InfraCorex: Infrastructure update from chat

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example_vm" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+resource "aws_s3_bucket" "enis_app_3" {
+  bucket = "enis_app_3"
+  acl    = "private"
 }
